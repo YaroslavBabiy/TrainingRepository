73f6e7cae26e4d695918382e770cf35a73cf5dd9
Ярослав Бабій <yaroslav.babiy01@gmail.com>
2019-08-11 14:36:10 +0300

    Add project files.

---
+++ Form1/Modal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Form1
+{
+    public class Modal
+    {
+        // клас який зберігає методи для обчеслення різних операцій
+
+        public string ConvertToHrn(decimal sumUsd)
+        {
+            return (sumUsd * 26.3m).ToString();
+        }
+
+        public string ConvertToUsd(decimal sumUah)
+        {
+            return (sumUah / 26.3m).ToString("F2");
+        }
+    }
+}
---
+++ Form1/Presenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Form1
+{
+    public class Presenter
+    {
+        Modal model = null;           //посилання на model
+        MainWindow mainWindow = null; // та mainWindow
+        public Presenter(MainWindow mainWindow)
+        {
+            model = new Modal();
+            this.mainWindow = mainWindow;
+            this.mainWindow.ToHrnEvent += MainWindow_toHrn; // підписує відповідну подію на відповідний метод-оброблювач
+        }
+        //метод-оброблювач
+        private void MainWindow_toHrn(object sender, EventArgs e)
+        {
+            if (mainWindow.textbox3.Text.ToUpper() == "UAH" && mainWindow.textbox4.Text.ToUpper() == "USD")
+            {
+                string varible = model.ConvertToHrn(Convert.ToDecimal(mainWindow.textBox1.Text)); // бере з моделі метод ConvertToHrn і в якості параметра передають конвертовану строку з mainWindow
+                mainWindow.textBox2.Text = varible;  // метод ConvertToHrn повертає якесь значення varible яке ми записуємо в textBox2       
+            }
+            else if (mainWindow.textbox3.Text.ToUpper() == "USD" && mainWindow.textbox4.Text.ToUpper() == "UAH")
+            {
+                string varible = model.ConvertToUsd(Convert.ToDecimal(mainWindow.textBox1.Text)); // бере з моделі метод ConvertToHrn і в якості параметра передають конвертовану строку з mainWindow
+                mainWindow.textBox2.Text = varible;  // метод ConvertToHrn повертає якесь значення varible яке ми записуємо в textBox2       
+            }
+            else mainWindow.textBox2.Text = "Вибачте за тимчасові не зручності, але дана функція не доступна на даний час. Команда наших розробників вже працює над цим";
+        }
+    }
+}
